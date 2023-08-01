41e367de7745dd2eb01407121cff55ee34f0cc10
WengOnn-Deriv <weng.onn@deriv.com>
2023-08-01 17:39:32 +0800

    modified precommit

--- pre-commit.sh
+++ pre-commit.sh
@@ -13,11 +13,11 @@ fi
 if git log -1 > /dev/null 2>&1; then
     trufflehog git file://. --no-update --since-commit HEAD --fail > /tmp/trufflehog_output_$(whoami) 2>&1
     trufflehog_exit_code=$?
-    echo $trufflehog_exit_code > /tmp/trufflehog_exit_code
+    echo $trufflehog_exit_code > /tmp/trufflehog_exit_code_$(whoami)
 else
     trufflehog filesystem . --no-update --fail > /tmp/trufflehog_output_$(whoami) 2>&1
     trufflehog_exit_code=$?
-    echo $trufflehog_exit_code > /tmp/trufflehog_exit_code
+    echo $trufflehog_exit_code > /tmp/trufflehog_exit_code_$(whoami)
 fi
 
 # Only display results to stdout if trufflehog found something.
