2b67ed0ebef8aacfc8c1ad7257b9b5dc7adbb489
ritik <ritik.karayat@deriv.com>
2023-08-01 16:33:19 +0800

    Final

--- pre-commit.sh
+++ pre-commit.sh
@@ -11,12 +11,10 @@ fi
 
 # Use `filesysytem` if the git repo does not have any commits i.e its a new git repo.
 if git log -1 > /dev/null 2>&1; then
-    echo "global gittt"
     trufflehog git file://. --no-update --since-commit HEAD --fail > /tmp/trufflehog_output_$(whoami) 2>&1
     trufflehog_exit_code=$?
     echo $trufflehog_exit_code > /tmp/trufflehog_exit_code
 else
-    echo "global fileystemmm"
     trufflehog filesystem . --no-update --fail > /tmp/trufflehog_output_$(whoami) 2>&1
     trufflehog_exit_code=$?
     echo $trufflehog_exit_code > /tmp/trufflehog_exit_code
