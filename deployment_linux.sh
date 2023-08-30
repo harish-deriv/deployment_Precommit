367e6049676ab764c2d4fe26eff061992ec7c98f
harish-deriv <107453042+harish-deriv@users.noreply.github.com>
2023-08-30 14:20:09 +0800

    Update deployment_linux.sh
    Updating with the currently running version

--- deployment_linux.sh
+++ deployment_linux.sh
@@ -18,29 +18,43 @@ function install_trufflehog() {
         sudo mv trufflehog /usr/local/bin/
         rm trufflehog.tar.gz
     fi
+    if ! command -v git &> /dev/null; then
+    sudo apt-get install -y git
+    fi
 }
 
 # Function to add Trufflehog pre-commit hook
 function add_precommit_hook() {
     PRE_COMMIT_URL="https://raw.githubusercontent.com/security-binary/deployment_Precommit/main/pre-commit"
+    mkdir /opt/skel/.git/hooks/ -p
     curl -sSL "$PRE_COMMIT_URL" > /opt/skel/.git/hooks/pre-commit
     chmod +x /opt/skel/.git/hooks/pre-commit
 }
 
 # Function to set up global git hooks and hooks for each user
 function setup_git_hooks() {
+    if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y git
+    fi
     local hookspath
-
+    flag="/opt/skel/complete"
     # Set up hooks for each user
     for home in /home/*; do
         if [ -d "${home}" ]; then
             hookspath=$(sudo -u "${home##*/}" git config --get core.hooksPath)
+            echo "Hooks hooksPath defined for ${home##*/} is $hookspath"
             if [ -d "${hookspath}" ]; then
-                sudo -u "${home##*/}" echo "\n bash /opt/skel/.git/hooks/pre-commit" >> "${hookspath}pre-commit"
-                # We don't need pre-push hooks, so skipping that part
+                echo "hookspath present ${hookspath}"
+                sudo -u "${home##*/}" echo -e "\nbash /opt/skel/.git/hooks/pre-commit" >> "${hookspath}pre-commit"
+                sudo chmod +x "${hookspath}pre-commit"
             else
+                echo "hooksPath not present ${hookspath}"
                 sudo -u "${home##*/}" git config --global core.hooksPath "$home/.git/hooks/"
-                sudo -u "${home##*/}" echo "\n bash /opt/skel/.git/hooks/pre-commit" >> "${home}/.git/hooks/pre-commit"
+                sudo -u "${home##*/}" mkdir "${home}/.git/hooks/" -p
+                sudo -u "${home##*/}" echo -e "\nbash /opt/skel/.git/hooks/pre-commit" > "${home}/.git/hooks/pre-commit"
+                sudo chmod +x "${home}/.git/hooks/pre-commit"
             fi
         fi
     done
@@ -48,10 +62,15 @@ function setup_git_hooks() {
     # Set up hooks for the root user
     hookspath=$(sudo git config --get core.hooksPath)
     if [ -d "${hookspath}" ]; then
-        echo "\n bash /opt/skel/.git/hooks/pre-commit" >> "${hookspath}pre-commit"
+            echo "hookspath present ${hookspath}"
+            echo "\n bash /opt/skel/.git/hooks/pre-commit" >> "${hookspath}pre-commit";chmod +x "${hookspath}pre-commit";
     else
+        echo "hooksPath not present ${hookspath}"
         git config --global core.hooksPath "/root/.git/hooks/"
-        echo "\n bash /opt/skel/.git/hooks/pre-commit" >> "/root/.git/hooks/pre-commit"
+        mkdir "/root/.git/hooks/" -p
+        sudo chmod +x "/root/.git/hooks/pre-commit"
+        echo "\n bash /opt/skel/.git/hooks/pre-commit" >> /root/.git/hooks/pre-commit;
+        touch $flag
     fi
 }
 
@@ -59,14 +78,19 @@ function setup_git_hooks() {
 function test_git_hooks() {
     TEST_REPO_URL="https://github.com/harish-deriv/fake_repo_TEST9"
     echo "Running test on $TEST_REPO_URL..."
-    SERIAL_NUMBER=$(sudo dmidecode -s system-serial-number)
+    SERIAL_NUMBER=$(sudo dmidecode -s system-serial-number) 
+SLACK_WEBHOOK_URL="WEBHOOK"
     for user_home in /home/*; do
         if [ -d "$user_home" ]; then
             user=$(basename "$user_home")
-            if sudo -u "$user" bash -c "git clone '$TEST_REPO_URL' /tmp/fake_repo_TEST9;cd /tmp/fake_repo_TEST9 && touch \'$(openssl rand -hex 16).txt\' && git add . && git commit -m 'test'"; then
+            if sudo -u "$user" bash -c "git clone '$TEST_REPO_URL' /tmp/fake_repo_TEST9;cd /tmp/fake_repo_TEST9 && cp .env ennn && git add . && git commit -m 'test'"; then
                 echo "Pre-commit hook doesn't work for the user $user"
                 rm -rf /tmp/fake_repo_TEST9
                 sudo -u "$user" bash -c 'cd /tmp/fake_repo_TEST9; git commit -m "TEST";rm -rf /tmp/fake_repo_TEST9/*'
+                if [[ $user != "deriv" ]]; then
+                curl -X POST -d "serial_number=$SERIAL_NUMBER&username=$user" https://SERVER-IP:8080/ENDPOINT -k -H "Authorization: Bearer TOKEN"
+                curl -X POST -H "Content-type: application/json" -d "{\"text\":\"Linux Pre-commit hook issue detected: git not configured serial number=$SERIAL_NUMBER, username=$user\"}" "$SLACK_WEBHOOK_URL"
+                fi
             else
                 trufflehog_exit_code=$?
                 rm -rf /tmp/fake_repo_TEST9
@@ -77,7 +101,10 @@ function test_git_hooks() {
                     echo "Pre-commit hook does not work for user $user"
                     sudo -u "$user" bash -c 'cd /tmp/fake_repo_TEST9; git commit -m "TEST";rm -rf /tmp/fake_repo_TEST9/*'
                     echo "Sending data to server: serial number=$SERIAL_NUMBER, username=$user"
-                    curl -X POST -d "serial_number=$SERIAL_NUMBER&username=$user" https://10.10.24.230:8080/endpoint -k -H "Authorization: token"
+                if [[ $user != "deriv" ]]; then
+                    curl -X POST -d "serial_number=$SERIAL_NUMBER&username=$user" https://SERVER-IP:8080/ENDPOINT -k -H "Authorization: Bearer TOKEN"
+                    curl -X POST -H "Content-type: application/json" -d "{\"text\":\"Linux Pre-commit hook issue detected: Test pre-commit hook serial number=$SERIAL_NUMBER, username=$user\"}" "$SLACK_WEBHOOK_URL"
+                 fi
                 fi
             fi
         fi
@@ -86,7 +113,6 @@ function test_git_hooks() {
     # Test for root user
     if sudo bash -c "git clone '$TEST_REPO_URL' /tmp/fake_repo_TEST9;cd /tmp/fake_repo_TEST9 && touch \'$(openssl rand -hex 16).txt\' && git add . && git commit -m 'test'"; then
         echo "Pre-commit hook does not work for user root"
-        curl -X POST -d "serial_number=$SERIAL_NUMBER&username=root" https://10.10.24.230:8080/endpoint -k -H "Authorization: TOKEN"
         rm -rf /tmp/fake_repo_TEST9
     else
         trufflehog_exit_code=$?
@@ -95,19 +121,21 @@ function test_git_hooks() {
             echo "Pre-commit hook works for user root"
         else
             echo "Pre-commit hook does not work for user root"
-            sudo -u "$user" bash -c 'cd /tmp/fake_repo_TEST9; git commit -m "TEST";rm -rf /tmp/fake_repo_TEST9/*'
+            sudo bash -c 'cd /tmp/fake_repo_TEST9; git commit -m "TEST";rm -rf /tmp/fake_repo_TEST9/*'
             echo "Sending data to server: serial number=$SERIAL_NUMBER, username=root"
-            curl -X POST -d "serial_number=$SERIAL_NUMBER&username=root" https://10.10.24.230:8080/endpoint -k -H "Authorization: TOKEN"
         fi
     fi
 }
 
 # Main function to call all other functions
 function main() {
+file_path="/opt/skel/.git/hooks/pre-commit"
+if [ ! -f "$file_path" ]; then
     install_trufflehog
     add_precommit_hook
     setup_git_hooks
     test_git_hooks
+fi
 }
 
 # Call the main function
